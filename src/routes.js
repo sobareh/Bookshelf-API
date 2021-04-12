f70b18431dac0c9065bf3c10ea3e5e7875115fa3
sobareh <Nestle2019>
2021-04-12 08:26:07 +0700

    Initial commit bookshelf-api submission

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
